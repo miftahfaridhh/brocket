ff80772bda37d0704b98e6df5641a22a6498f434
miftahfaridhh <muhammadmiftahfaridh@gmail.com>
2021-04-08 10:52:09 +0700

    podium

--- app/Http/Controllers/ShowController.php
+++ app/Http/Controllers/ShowController.php
@@ -144,9 +144,9 @@ class ShowController extends Controller
     public function podium($id)
     {
         
-        return $members = DB::table('member')
+        $members = DB::table('member')
                 ->where('turnamen_id', '=', $id)
-                ->orderBy('created_at', 'desc')->get();
+                ->orderBy('total_medal', 'desc')->get();
         // dd($members);
 
         $turney = DB::table('turnamen')
